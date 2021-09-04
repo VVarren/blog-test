58825a3ec5b62f12265648199e0473bd962e8ac8
VVarren <wwu2572@flhs.us>
2021-09-04 17:34:06 -0400

    i am sick2

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import React, { useState } from "react";
 
 function App() {
 	const [count, setCount] = useState(0);
+	const [title, settitle] = useState("");
 	return (
-		<div>
-			<button onClick={() => setCount(count + 1)}>{count}</button>
-			<button onClick={() => setCount(count - 1)}>{count}</button>
+		<div className="flex-container">
+			<input
+				placeholder="Whats the title?"
+				onChange={(e) => settitle(e.target.value)}
+				value={title}
+			></input>
+			<textarea rows="5" cols="33"></textarea>
+			<button className="add" onClick={() => setCount()}>
+				+ Add the blog +
+			</button>
 		</div>
 	);
 }
